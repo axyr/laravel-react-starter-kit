8d13de723696575a7504b63903bdf4893feb3831
Martijn van Nieuwenhoven <martijn@novat.io>
2025-07-12 09:51:15 +0200

    hm

--- app/Console/Commands/FrontendInstall.php
+++ app/Console/Commands/FrontendInstall.php
@@ -19,8 +19,6 @@ class FrontendInstall extends Command
 
     public function handle(): void
     {
-        $this->call('config:clear');
-
         $targetDir = base_path($this->option('directory'));
         $repoUrl = $this->option('repository');
         $fresh = $this->option('fresh');
